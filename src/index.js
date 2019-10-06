6ec80ef291a081dc9569b0e475efe5b7baa1fb65
Alexandr_Dvoryadkin <dvoryadkin66@mail.ru>
2019-10-07 00:15:50 +0400

    Реализовано модальное окно и табы два

---
+++ src/index.js
@@ -0,0 +1,11 @@
+ 'use strict';
+
+ import togglePopup from './modules/togglePopup';
+ import tabPanel from './modules/tabPanel';
+
+
+ //Popup Window
+ togglePopup();
+ //Табы
+ tabPanel();
+
---
+++ src/modules/tabPanel.js
@@ -0,0 +1,34 @@
+const tabPanel = () => {
+
+  const accordionTwo = document.getElementById('accordion-two'),
+    panel = accordionTwo.querySelectorAll('.panel'),
+    panelCollapse = document.querySelectorAll('#accordion-two .panel > .panel-collapse');
+
+  const toggleTabContent = (index) => {
+    for (let i = 0; i < panelCollapse.length; i++) {
+      if (index === i) {
+        panelCollapse[i].classList.add('in');
+      } else {
+        panelCollapse[i].classList.remove('in');
+      }
+    }
+  };
+  //toggleTabContent(2);
+  accordionTwo.addEventListener('click', (event) => {
+    let target = event.target;
+    console.log('target: ', target);
+
+    target = target.closest('.panel');
+    console.log('target: ', target);
+    if (target) {
+      panel.forEach((item, i) => {
+        if (item === target) {
+          toggleTabContent(i);
+        }
+      });
+    }
+  });
+
+};
+
+export default tabPanel;
