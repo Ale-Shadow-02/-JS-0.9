f47025aa6b770d9c7121078cdde262b4635d44b5
Ale-Shadow-02 <dvoryadkiin@yandex.ru>
2019-10-27 08:42:37 +0400

    Biuty

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
- 'use strict';
+'use strict';
 
- import togglePopup from './modules/togglePopup';
- import tabPanel from './modules/tabPanel';
- import sendForm1 from './modules/sendForm1';
- import sentence from './modules/sentence';
- import gauging from './modules/gauging';
- import calc from './modules/calc';
+import togglePopup from './modules/togglePopup';
+import tabPanel from './modules/tabPanel';
+import sendForm1 from './modules/sendForm1';
+import sentence from './modules/sentence';
+import gauging from './modules/gauging';
+import calc from './modules/calc';
 
 
- //Popup Window
- togglePopup();
- //Табы
- tabPanel();
- //Отправка формы модального окна
- sendForm1();
- //Секция Акции и предложения
- sentence();
- //Секция Закажите бесплатный замер
- gauging();
- //Калькулятор-аккоредон
- calc();
+//Popup Window
+togglePopup();
+//Табы
+tabPanel();
+//Отправка формы модального окна
+sendForm1();
+//Секция Акции и предложения
+sentence();
+//Секция Закажите бесплатный замер
+gauging();
+//Калькулятор-аккоредон
+calc();
